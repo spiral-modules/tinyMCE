518500d49c3e57fdb10a26adb64ed09e2d802492
iasinov <iasinov@gmail.com>
2016-02-10 17:54:05 +0300

    form.editor tag

--- source/views/tinymce-bundle.dark.php
+++ source/views/tinymce-bundle.dark.php
@@ -1,2 +1,4 @@
 <dark:use path="spiral:tinymce" as="form.tinymce"/>
 <dark:use path="spiral:tinymce" as="form:tinymce"/>
+<dark:use path="spiral:tinymce" as="form:editor"/>
+<dark:use path="spiral:tinymce" as="form.editor"/>
